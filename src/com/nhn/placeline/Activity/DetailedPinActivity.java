23448e5b7d7e5f811df1ced00d0a9d10c8f3ed2a
junsun <junsun2005@naver.com>
2013-01-09 02:34:22 +0900

    detailed pin activity

---
+++ src/com/nhn/placeline/Activity/DetailedPinActivity.java
@@ -0,0 +1,33 @@
+package com.nhn.placeline.Activity;
+
+import java.util.ArrayList;
+
+import com.nhn.placeline.Activity.R;
+import com.nhn.placeline.util.GroupAdapter;
+import com.nhn.placeline.vo.Group;
+import com.nhn.android.mapviewer.NMapViewer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class DetailedPinActivity extends Activity {
+
+	private ArrayList<Group> groups;
+	private String userid = "oskar";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detailed_pin);
+		
+		
+		
+		
+	}
+}
